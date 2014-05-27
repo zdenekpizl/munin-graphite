eee373d1f82eceee54b2e520e93e15cda992a61b
Zdenek Pizl <zdenek.pizl@gooddata.com>
2014-05-27 15:22:00 +0200

    FEATURE: PCI-3817 prepare tooling for munin-node plugin config parsing
    - refactor of munin class to specific file
    - main m2g-poller.py is now importing m2g_munin-thread "module"

--- m2g_getconfig.py
+++ m2g_getconfig.py
@@ -5,20 +5,20 @@ import argparse
 import ConfigParser
 import logging
 import logging.handlers
+import pickle
 import re
+import socket
+import struct
 import sys
 import time
 import signal
+import threading
 import m2g_munin_thread
 
 RE_LEFTRIGHT = re.compile(r"^(?P<left>\S+)\s+(?P<right>\S+)$")
 RE_MUNIN_NODE_NAME = re.compile(r"^# munin node at\s+(?P<nodename>\S+)$")
 
-threads = []
 
-###
-# bellow are common function
-###
 def parse_args():
     """Parse command line arguments."""
     parser = argparse.ArgumentParser(description="Send Munin statistics to Graphite.")
@@ -71,26 +71,6 @@ def parse_args():
     return args
 
 
-###
-# stop all threads and exit
-###
-def handler_term(signum=signal.SIGTERM, frame=None):
-    global threads
-
-    for t in threads:
-        t.dostop()
-
-
-###
-# set all threads to reload information about all munin-node's plugins
-###
-def handler_hup(signum, frame=None):
-    global threads
-
-    for t in threads:
-        t.reload()
-
-
 def read_configuration(configfile):
     """
     Returns False if configuration file is not readable, list of dictionaries otherwise
@@ -131,66 +111,8 @@ def read_configuration(configfile):
 
     return hostscfg
 
-def list_plugins(self):
-    """Return a list of Munin plugins configured on a node. """
-    self._sock.sendall("cap multigraph\n")
-    self._readline()  # ignore response
-
-    if self.remotenode:
-        self.thread.logger.info("Thread %s: Asking for plugin list for remote node %s", self.thread.name, self.remotenode)
-        self._sock.sendall("list %s\n" % self.remotenode)
-    else:
-        self.thread.logger.info("Thread %s: Asking for plugin list for local node %s", self.thread.name, self.hostname)
-        self._sock.sendall("list\n")
-
-    plugin_list = self._readline().split(" ")
-    if self.args.filter:
-        try:
-            filteredlist = [plugin for plugin in plugin_list if re.search(self.args.filter, plugin, re.IGNORECASE)]
-            plugin_list = filteredlist
-        except re.error:
-            self.thread.logger.info("Thread %s: Filter regexp for plugin list is not valid: %s" % self.args.filter)
-        # if there is no filter or we have got an re.error, simply return full list
-    result_list = []
-    for plugin in plugin_list:
-        if len(plugin.strip()) > 0:
-            result_list.append(plugin)
-    return result_list
-
-def get_config(self, plugin):
-    """Get config values for Munin plugin."""
-    self._sock.sendall("config %s\n" % plugin)
-    response = {None: {}}
-    multigraph = None
-
-    for current_line in self._iterline():
-        if current_line.startswith("multigraph "):
-            multigraph = current_line[11:]
-            response[multigraph] = {}
-            continue
-
-        try:
-            key_name, key_value = current_line.split(" ", 1)
-        except ValueError:
-            # ignore broken plugins that don't return a value at all
-            continue
-
-        if "." in key_name:
-            # Some keys have periods in them.
-            # If so, make their own nested dictionary.
-            key_root, key_leaf = key_name.split(".", 1)
-            if key_root not in response:
-                response[multigraph][key_root] = {}
-            response[multigraph][key_root][key_leaf] = key_value
-        else:
-            response[multigraph][key_name] = key_value
-
-    return response
-
-
 
 def main():
-    global threads
     global logger
 
     args = parse_args()
@@ -213,11 +135,6 @@ def main():
     else:
         logger.addHandler(stdout)
 
-    # block for setting handling of signals
-    signal.signal(signal.SIGHUP, handler_hup)
-    signal.signal(signal.SIGTERM, handler_term)
-    signal.signal(signal.SIGINT, handler_term)
-
     hosts = list()
     if args.config:
         hosts = read_configuration(args.config)
@@ -225,22 +142,10 @@ def main():
         # no file configuration, trying to use commandline arguments only and construct one-item dictionary
         hosts.append({'host': args.host})
         # we have got some items in hosts's list
+
+    thread = m2g_munin_thread.MuninThread(host, args, logger)
     for host in hosts:
-        logging.info("Going to thread with config %s" % host)
-        threads.append(m2g_munin_thread.MuninThread(host, args, logger))
-
-    for t in threads:
-        t.start()
-
-    while True:
-        try:
-            if not any([t.isAlive() for t in threads]):
-                logging.info("All threads finished, exiting.")
-                break
-            else:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            handler_term()
+        m = thread.munin
 
 
 if __name__ == '__main__':
---
+++ m2g_munin_thread.py
@@ -0,0 +1,340 @@
+#!/usr/bin/python
+"""Gather Munin statistics and deliver to Carbon for Graphite display."""
+
+import argparse
+import pickle
+import re
+import socket
+import struct
+import sys
+import time
+import logging
+import threading
+
+RE_LEFTRIGHT = re.compile(r"^(?P<left>\S+)\s+(?P<right>\S+)$")
+RE_MUNIN_NODE_NAME = re.compile(r"^# munin node at\s+(?P<nodename>\S+)$")
+
+
+class Munin():
+    """Munin host object with querying getter functions."""
+
+    def __init__(self, hostname, logger=None, port=4949, args=None):
+
+        self.hostname = None
+        self.remotenode = None
+        self._sock = None
+        self._conn = None
+        self._carbon_sock = None
+        self.hello_string = None
+        self.reload_plugins = True
+        self.shutdown = False
+        self.plugins = {}
+        self.plugins_config = {}
+        self.port = port
+        self.args = args
+
+        if ':' in hostname:
+            self.hostname, self.remotenode = hostname.split(":", 1)
+        else:
+            self.hostname = hostname
+
+        if self.args.displayname:
+            self.displayname = self.args.displayname.split(".")[0]
+        else:
+            self.displayname = self.hostname.split(".")[0]
+
+        if logger:
+            self.logger = logger
+        else:
+            # if we have not provided a logger, let's create a very basic one
+            self.logger = logging.getLogger()
+
+    def set_shutdown(self,newstatus):
+        """Updating shutdown status from connection hello string."""
+        self.shutdown = newstatus
+
+    def set_reload_plugins(self,newstatus):
+        """Updating shutdown status from connection hello string."""
+        self.reload_plugins = newstatus
+
+    def go(self):
+        """Bootstrap method to start processing hosts's Munin stats."""
+        self.connect()
+        self.update_hostname()
+        processing_time = self.process_host_stats()
+        interval = int(self.args.interval)
+
+        while True and interval != 0 and not self.shutdown:
+            sleep_time = max(interval - processing_time, 0)
+            time.sleep(sleep_time)
+            self.connect()
+            processing_time = self.process_host_stats()
+
+    def update_hostname(self):
+        """Updating hostname from connection hello string."""
+        if self.args.displayname:
+            return
+        try:
+            node_name = RE_MUNIN_NODE_NAME.search(self.hello_string).group(1)
+            self.displayname = node_name.split(".")[0]
+        except AttributeError:
+            self.logger.info("Thread %s: Unable to obtain munin node name from: %s",
+                        self.hostname, self.hello_string)
+            return
+
+    def connect(self):
+        """Initial connection to Munin host."""
+        try:
+            self._sock = socket.create_connection((self.hostname, self.port), 10)
+        except socket.error:
+            self.logger.exception("Thread %s: Unable to connect to Munin host %s, port: %s",
+                             self.hostname, self.hostname, self.port)
+            sys.exit(1)
+
+        try:
+            self._conn = self._sock.makefile()
+            self.hello_string = self._readline()
+        except socket.error:
+            self.logger.exception("Thread %s: Unable to communicate to Munin host %s, port: %s",
+                             self.hostname, self.hostname, self.port)
+
+        if self.args.carbon:
+            self.connect_carbon()
+
+    def connect_carbon(self):
+        carbon_host, carbon_port = self.args.carbon.split(":")
+        try:
+            self._carbon_sock = socket.create_connection((carbon_host, carbon_port), 10)
+        except socket.error:
+            self.logger.exception("Thread %s: Unable to connect to Carbon on host %s, port: %s",
+                             self.hostname, carbon_host, carbon_port)
+            sys.exit(1)
+
+    def close_connection(self):
+        """Close connection to Munin host."""
+        self._sock.close()
+
+    def close_carbon_connection(self):
+        """Close connection to Carbon host."""
+        if self._carbon_sock:
+            self._carbon_sock.close()
+
+    def _readline(self):
+        """Read one line from Munin output, stripping leading/trailing chars."""
+        return self._conn.readline().strip()
+
+    def _iterline(self):
+        """Iterator over Munin output."""
+        while True:
+            current_line = self._readline()
+            self.logger.debug("Thread %s: Iterating over line: %s", self.hostname, current_line)
+            if not current_line:
+                break
+            if current_line.startswith("#"):
+                continue
+            if current_line == ".":
+                break
+            yield current_line
+
+    def fetch(self, plugin):
+        """Fetch plugin's data fields from Munin."""
+        self._sock.sendall("fetch %s\n" % plugin)
+        response = {None: {}}
+        multigraph = None
+        multigraph_prefix = ""
+        for current_line in self._iterline():
+            if current_line.startswith("multigraph "):
+                multigraph = current_line[11:]
+                multigraph_prefix = multigraph.rstrip(".") + "."
+                response[multigraph] = {}
+                continue
+                # Some munin plugins have more than one space between key and value.
+            try:
+                full_key_name, key_value = RE_LEFTRIGHT.search(current_line).group(1, 2)
+                key_name = multigraph_prefix + full_key_name.split(".")[0]
+                response[multigraph][key_name] = key_value
+            except (KeyError, AttributeError):
+                self.logger.info("Thread %s: Plugin %s returned invalid data [%s] for host"
+                            " %s\n", self.hostname, plugin, current_line, self.hostname)
+
+        return response
+
+    def list_plugins(self):
+        """Return a list of Munin plugins configured on a node. """
+        self._sock.sendall("cap multigraph\n")
+        self._readline()  # ignore response
+
+        if self.remotenode:
+            self.logger.info("Thread %s: Asking for plugin list for remote node %s", self.hostname, self.remotenode)
+            self._sock.sendall("list %s\n" % self.remotenode)
+        else:
+            self.logger.info("Thread %s: Asking for plugin list for local node %s", self.hostname, self.hostname)
+            self._sock.sendall("list\n")
+
+        plugin_list = self._readline().split(" ")
+        if self.args.filter:
+            try:
+                filteredlist = [plugin for plugin in plugin_list if re.search(self.args.filter, plugin, re.IGNORECASE)]
+                plugin_list = filteredlist
+            except re.error:
+                self.logger.info("Thread %s: Filter regexp for plugin list is not valid: %s" % self.args.filter)
+            # if there is no filter or we have got an re.error, simply return full list
+        result_list = []
+        for plugin in plugin_list:
+            if len(plugin.strip()) > 0:
+                result_list.append(plugin)
+        return result_list
+
+    def get_config(self, plugin):
+        """Get config values for Munin plugin."""
+        self._sock.sendall("config %s\n" % plugin)
+        response = {None: {}}
+        multigraph = None
+
+        for current_line in self._iterline():
+            if current_line.startswith("multigraph "):
+                multigraph = current_line[11:]
+                response[multigraph] = {}
+                continue
+
+            try:
+                key_name, key_value = current_line.split(" ", 1)
+            except ValueError:
+                # ignore broken plugins that don't return a value at all
+                continue
+
+            if "." in key_name:
+                # Some keys have periods in them.
+                # If so, make their own nested dictionary.
+                key_root, key_leaf = key_name.split(".", 1)
+                if key_root not in response:
+                    response[multigraph][key_root] = {}
+                response[multigraph][key_root][key_leaf] = key_value
+            else:
+                response[multigraph][key_name] = key_value
+
+        return response
+
+    def process_host_stats(self):
+        """Process Munin node data, potentially sending to Carbon."""
+        start_timestamp = time.time()
+        self.logger.info("Thread %s: Querying host %s", self.hostname, self.hostname)
+        # to be more efficient, load list of plugins just in case we do not have any
+        if self.reload_plugins:
+            self.plugins_config = {}
+            self.plugins = self.list_plugins()
+            self.set_reload_plugins(False)
+            self.logger.debug("Thread %s: Plugin List: %s", self.hostname, self.plugins)
+        epoch_timestamp = int(start_timestamp)
+
+        for current_plugin in self.plugins:
+            self.logger.info("Thread %s: Fetching plugin: %s (Host: %s)",
+                        self.hostname, current_plugin, self.hostname)
+
+            # after (re)load of list of plugins we have to load their configurations too
+            try:
+                self.plugins_config[current_plugin]
+            except KeyError:
+                self.plugins_config[current_plugin] = self.get_config(current_plugin)
+                self.logger.debug("Thread %s: Plugin Config: %s", self.hostname, self.plugins_config[current_plugin])
+
+            plugin_data = self.fetch(current_plugin)
+            self.logger.debug("Thread %s: Plugin Data: %s", self.hostname, plugin_data)
+            if self.args.carbon:
+                for multigraph in self.plugins_config[current_plugin]:
+                    try:
+                        self.send_to_carbon(epoch_timestamp,
+                                            current_plugin,
+                                            self.plugins_config[current_plugin][multigraph],
+                                            plugin_data[multigraph])
+                    except KeyError:
+                        self.logger.info("Thread %s: Plugin returns invalid data:\n plugin_config: %r host %s.",
+                                    self.hostname, self.plugins_config[current_plugin], self.hostname)
+        end_timestamp = time.time() - start_timestamp
+        self.close_connection()
+        self.close_carbon_connection()
+        self.logger.info("Thread %s: Finished querying host %s (Execution Time: %.2f sec).",
+                    self.hostname, self.hostname, end_timestamp)
+        return end_timestamp
+
+    def send_to_carbon(self, timestamp, plugin_name, plugin_config, plugin_data):
+        """Send plugin data to Carbon over Pickle format."""
+        if self.args.noprefix:
+            prefix = ''
+        else:
+            prefix = "%s." % self.args.prefix
+
+        hostname = self.hostname
+        if self.remotenode:
+            hostname = self.remotenode
+
+        data_list = []
+        self.logger.info("Creating metric for plugin %s, timestamp: %d",
+                    plugin_name, timestamp)
+
+        for data_key in plugin_data:
+            try:
+                plugin_category = plugin_config["graph_category"]
+                metric = "%s%s.%s.%s.%s" % (prefix, self.displayname, plugin_category, plugin_name, data_key)
+                value = plugin_data[data_key]
+                self.logger.debug("Creating metric %s, value: %s", metric, value)
+                data_list.append((metric, (timestamp, value)))
+            except KeyError:
+                self.logger.info("plugin returns invalid data:\n plugin_config: %r host %s.", plugin_config, self.hostname)
+
+        if self.args.noop:
+            self.logger.info("NOOP: Not sending data to Carbon")
+            return
+
+        self.logger.info("Sending plugin %s data to Carbon for host %s.", plugin_name, hostname)
+        payload = pickle.dumps(data_list)
+        header = struct.pack("!L", len(payload))
+        message = header + payload
+        try:
+            self._carbon_sock.sendall(message)
+            self.logger.info("Finished sending plugin %s data to Carbon for host %s.", plugin_name, self.hostname)
+        except socket.error:
+            self.logger.exception("Unable to send data to Carbon")
+
+
+###
+# Custom Threading class, one thread for each host in configuration
+###
+class MuninThread(threading.Thread):
+    def __init__(self, params, cmdlineargs, logger=None):
+        threading.Thread.__init__(self)
+        self.name = params['host']
+
+        # construct new namespace to pass it to the new Munin class instance
+        # for better manipulation, just prepare writable dcfg "link" to new namespace
+        cfg = argparse.Namespace()
+        dcfg = vars(cfg)
+
+        #construct final arguments Namespace
+        for v in vars(cmdlineargs):
+            try:
+                dcfg[v] = params[v]
+            except KeyError:
+                dcfg[v] = getattr(cmdlineargs, v, None)
+
+        if logger:
+            self.logger = logger
+        else:
+            # if we have not provided a logger, let's create a very basic one
+            self.logger = logging.getLogger()
+
+        self.munin = Munin(hostname=self.name, args=cfg, logger=self.logger)
+
+    def run(self):
+        self.logger.info("Starting thread for %s." % self.name)
+        self.munin.go()
+        self.logger.info("Finishing thread for %s." % self.name)
+
+    def dostop(self):
+        self.logger.info("Thread %s: Got signal to stop." % self.name)
+        self.munin.set_shutdown(True)
+
+    def reload(self):
+        self.logger.info("Thread %s: Got signal to reload." % self.name)
+        self.munin.set_reload_plugins(True)
+
